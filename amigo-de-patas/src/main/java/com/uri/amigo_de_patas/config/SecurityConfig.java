4b1acf616cc50dd74e6f9af2d2fd3910ca6170fb
jornadev <jornadahenrique12@gmail.com>
2025-04-25 20:20:57 -0300

    [feat] | implementing UUID

--- amigo-de-patas/src/main/java/com/uri/amigo_de_patas/config/SecurityConfig.java
+++ amigo-de-patas/src/main/java/com/uri/amigo_de_patas/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // libere o front-end
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
