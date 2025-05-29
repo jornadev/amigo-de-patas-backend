57bcfb8d060e72f5ee19b7a50fdf0d7dfa5d9a5b
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-05-29 11:47:58 -0300

    feat: created Application model

---
+++ amigo-de-patas/src/main/java/com/uri/amigo_de_patas/model/Application.java
@@ -0,0 +1,42 @@
+package com.uri.amigo_de_patas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "applications")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @ManyToOne(optional = false)
+    private Animal animal;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationType type;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status = ApplicationStatus.PENDENTE;
+
+    private String userName;
+    private String address;
+    private String phone;
+
+    private String message;
+
+    private LocalDateTime applicationDate = LocalDateTime.now();
+
+}
+
